44ece71f620ab1fd07ded502f94de3b1ac61181a
yurashalya <stallker9696@gmail.com>
2024-08-31 17:36:56 +0300

    Added global tailwind settings + created Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 const Home = () => {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home Page
-    </main>
-  );
+  return <main>Homepage</main>;
 };
 
 export default Home;
