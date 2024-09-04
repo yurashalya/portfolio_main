a5a680042e0ec1a028b0588f95bf17f831d82577
yurashalya <stallker9696@gmail.com>
2024-09-05 02:09:51 +0300

    Settings metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={JetBrainsMono.variable}>
         <Header />
         <StairTransition />
